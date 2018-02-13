713ad51efc922e234a430796735b5e4475d60664
SeokWon Hong <h20913@gmail.com>
2018-02-13 13:32:41 +0900

    test impl

---
+++ MovieInfoGather.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MovieInfoGather.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MovieInfoGather.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8bb3a161-eb48-4207-a8e4-72e8f7bff878")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ MovieInfoGather.Test/UnitTest1.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MovieInfoGather.Repositories;
+using MovieInfoGather.Repositories.Contract;
+using System.Threading.Tasks;
+
+namespace MovieInfoGather.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        IKobisRepository kobisRepo;
+
+        public UnitTest1()
+        {
+            kobisRepo = new KobisRepository();
+        }
+
+        [TestMethod]
+        public async Task TestMethod1()
+        {
+            var result = await kobisRepo.GetBoxOfficeAsync(new Models.KobisBoxOfficeRequest()
+            {
+                key = Consts.KOBIS_API_KEY,
+                targetDt = "20180102",
+
+            });
+
+            Assert.AreNotEqual(0, result.boxOfficeResult.dailyBoxOfficeList.Count);
+        }
+
+        [TestMethod]
+        public async Task TestMethod2()
+        {
+            var result = await kobisRepo.GetMovieListAsync(new Models.KobisMovieListRequest
+            {
+                key = Consts.KOBIS_API_KEY,
+                openStartDt = "2018",
+                openEndDt = "2018"
+            });
+
+            Assert.AreNotEqual(0, result.movieListResult.movieList.Count);
+
+            foreach(var movie in result.movieListResult.movieList)
+            {
+                var subresult = await kobisRepo.GetMovieDetailAsync(new Models.KobisMovieInfoDetailRequest
+                {
+                    key = Consts.KOBIS_API_KEY,
+                    movieCd = movie.movieCd
+                });
+
+                Assert.AreNotEqual("", subresult.movieInfoResult.movieInfo.movieNm);
+
+                break;
+            }
+        }
+    }
+}
